c8b7937363f20f81ece3590ff957bbfc5e86cc14
zhangyu <11010170+firzhangyu@user.noreply.gitee.com>
2022-10-18 20:54:51 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,157 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      <div style="">
+        <img style="height:8vh"  src="../assets/logo.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header
+    >
+    <!-- 主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' :'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+        :default-active="activePath"
+      router
+      :collapse-transition="false"
+      unique-opened
+      :collapse="isCollapse"
+      background-color="#333744"
+      text-color="#fff"
+      active-text-color="#409EFF">
+      <!-- 一级菜单 -->
+      <!-- index控制点击后的展开，index只接收字符串  -->
+      <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+        <!-- 一级菜单模板区域 -->
+        <template slot="title">
+          <!-- 图标 -->
+          <i class="iconfont" :class="iconObj[item.id]"></i>
+          <!-- 文本 -->
+          <span>{{item.authName}}</span>
+        </template>
+        <!-- 二级菜单 -->
+        <el-menu-item @click="saveNavState('/'+item2.path)" :index="'/'+item2.path" v-for="item2 in item.children" :key="item2.id">
+          <template slot="title">
+          <!-- 图标 -->
+          <i class="el-icon-menu"></i>
+          <!-- 文本 -->
+          <span>{{item2.authName}}</span>
+        </template></el-menu-item>
+       
+      </el-submenu>
+     
+    </el-menu>
+  
+      </el-aside>
+      <!-- 右侧内容布局 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name:'HomePart',
-    methods:{
-      // 退出
-      logout(){
-        sessionStorage.clear('token')
-        this.$router.push('/login')
+  name: "HomePart",
+  data(){
+    return {
+      // 左侧菜单数据
+      menuList:[],
+      iconObj:{
+        '125':'el-icon-user-solid',
+        '103':'el-icon-lock',
+        '101':'el-icon-s-shop',
+        '102':'el-icon-date',
+        '145':'el-icon-s-data'
+      },
+      // 是否折叠
+      isCollapse:false,
+      // 激活链接地址
+      activePath:''
 
-      }
     }
 
+  },
+  created(){
+   this.getMenuList()
+   this.activePath = sessionStorage.getItem('activePath')
+  },
+  methods: {
+    // 退出
+    logout() {
+      sessionStorage.clear("token");
+      this.$router.push("/login");
+    },
+    // 获取所有菜单
+   async getMenuList(){
+    const {data:res} =  await this.$http.get('menus')
+    if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+    this.menuList = res.data
+    console.log(res);
+    },
+  // 控制侧边栏得展开与折叠
+    toggleCollapse(){
+      this.isCollapse = !this.isCollapse
 
-}
+    },
+    // 获取路径名在会话中存储，保证连接的激活状态
+    saveNavState(activePath){
+      sessionStorage.setItem('activePath',activePath)
+      this.activePath = activePath
+    }
+  },
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 20px;
+    }
+  }
+}
 
+.el-aside {
+  background-color: #333744;
+  .el-menu{
+    border: 0;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button{
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color:#fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+  
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+// 请求拦截器request在请求前调用config函数
+axios.interceptors.request.use(config =>{
+  // 挂载了一个请求头来区分权限
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
